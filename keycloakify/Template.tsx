e6561125a634ed847b497efff4c6c5a1d8403a72
Martin Fenner <martin@front-matter.io>
2025-12-27 14:25:53 +0100

    add custom template

---
+++ keycloakify/Template.tsx
@@ -0,0 +1,44 @@
+Theme type:
+→ login
+Select the page you want to customize:
+[?25l(x) Template.tsx (Layout common to every page)
+( ) UserProfileFormFields.tsx (Renders the form of the register.ftl, login-update-profile.ftl, update-email.ftl and idp-review-user-profile.ftl)
+( ) login.ftl
+( ) login-username.ftl
+( ) login-password.ftl
+( ) webauthn-authenticate.ftl
+( ) webauthn-register.ftl
+( ) register.ftl
+( ) info.ftl
+( ) error.ftl
+( ) login-reset-password.ftl
+( ) login-verify-email.ftl
+( ) terms.ftl
+( ) login-oauth2-device-verify-user-code.ftl
+( ) login-oauth-grant.ftl
+( ) login-otp.ftl
+( ) login-update-profile.ftl
+( ) login-update-password.ftl
+( ) link-idp-action.ftl
+( ) login-idp-link-confirm.ftl
+( ) login-idp-link-email.ftl
+( ) login-page-expired.ftl
+( ) login-config-totp.ftl
+( ) logout-confirm.ftl
+( ) idp-review-user-profile.ftl
+( ) update-email.ftl
+( ) select-authenticator.ftl
+( ) saml-post-form.ftl
+( ) delete-credential.ftl
+( ) code.ftl
+( ) delete-account-confirm.ftl
+( ) frontchannel-logout.ftl
+( ) login-recovery-authn-code-config.ftl
+( ) login-recovery-authn-code-input.ftl
+( ) login-reset-otp.ftl
+( ) login-x509-info.ftl
+( ) webauthn-error.ftl
+( ) login-passkeys-conditional-authenticate.ftl
+( ) login-idp-link-confirm-override.ftl
+( ) select-organization.ftl
+( ) The page you're looking for isn't listed here[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[1A[2K[G[?25h
